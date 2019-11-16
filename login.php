ad857b577c14893f2cae07f4efeff68d3ca5cfe1
Rémi Durieu <remi.durieu@univ-rouen.fr>
2019-11-16 21:39:35 +0000

    Implante la connexion d'un utilisateur enregistré
      - index.html devient index.php
      - ajout d'une session lors de la navigation
      - à la connexion, l'utilisateur est redirigé vers index.php
      - si un utilisateur est connecté, les boutons de connexion et de
      déconnexion disparaissent au profit d'un message d'accueil
    
    À FAIRE :
      - ajouter un bouton de déconnexion

--- login.php
+++ login.php
@@ -1,3 +1,44 @@
+<?php
+session_start();
+
+include "db_infos.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
+  $email = test_input($_POST["email"]);
+  $password = test_input($_POST["password"]);
+  $err_msg = "";
+
+  // Créé une connexion à la base de donnée avec les informations contenues dans
+  //    le fichier db_infos.php
+  $connection = mysqli_connect($db_server, $db_username, $db_password, $db_database);
+  if (!$connection) {
+    exit ("Connection failed :".mysqli_connect_error());
+  }
+
+  // Envoie une requête à la base de données, recherchant l'entrée qui
+  //    correspond au couple email/password donnée en entrée
+  $request = "SELECT * FROM $db_table WHERE email = '$email' AND password = '$password'";
+  $result = mysqli_query($connection, $request);
+  if (mysqli_fetch_assoc($result) == NULL) {
+    $err_msg = "L'adresse mail et/ou le mot de passe sont incorrects.";
+  } else {
+    $_SESSION["user_email"] = $email;
+    header("Location: index.php");
+  }
+
+  // Ferme la connexion
+  mysqli_close($connection);
+}
+?>
+
 <!DOCTYPE html>
 
 <html lang="fr">
@@ -11,10 +52,16 @@
 
   <body>
     <header>
-      <a href="index.html" id="page-title">mem.io</a>
+      <a href="index.php" id="page-title">mem.io</a>
       <div id="user-authentification">
-        <a href="login.php">Connexion</a>
-        <a href="register.php">Nouveau compte</a>
+        <?php
+        if (isset($_SESSION["user_email"])) {
+          echo "<p>Bienvenue ".$_SESSION["user_email"]."</p>";
+        } else {
+          echo "<a href=\"login.php\">Connexion</a>";
+          echo "<a href=\"register.php\">Nouveau compte</a>";
+        }
+        ?>
       </div>
     </header>
 
@@ -22,11 +69,15 @@
       <h2>Connexion</h2>
       <br>
       <form method="POST" name="login_form" action="<?php echo $_SERVER["PHP_SELF"];?>">
-        E-mail :<br>
+        E-mail :
+        <br>
         <input type="email" name="email" placeholder="jean.dupont@exemple.com" required>
         <br>
-        Mot de passe :<br>
-        <input type="password" name="password" placeholder="**********" required><br>
+        Mot de passe :
+        <br>
+        <input type="password" name="password" placeholder="**********" required>
+        <br>
+        <?php echo $err_msg; ?>
         <br>
         <input type="submit" value="Se connecter">
       </form>
@@ -35,13 +86,10 @@
     <footer>
       <p>"Memory.io" par Rémi Durieu et Thomas Evrard sous licence MIT
         <a target="_blank" href="https://github.com/tef76/memory_web">
-        <img id="github-logo"
-          src="img/GitHub-Mark-Light-32px.png"
-          alt="GitHub Logo">
+          <img id="github-logo" src="img/GitHub-Mark-Light-32px.png" alt="GitHub Logo">
         </a>
       </p>
-      <p>Set de cartes "Playing Cards" par Iron Star Media sous licence CC-BY
-        3.0 modifié par Rémi Durieu</p>
+      <p>Set de cartes "Playing Cards" par Iron Star Media sous licence CC-B 3.0 modifié par Rémi Durieu</p>
       <p>Police "Comfortaa" par Johan Aakerlund sous licence OFL</p>
     </footer>
   </body>
