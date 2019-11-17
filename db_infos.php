d03d7e25b4729dd3e7370fc7ac6ce27bd3b793fd
Rémi Durieu <remi.durieu@univ-rouen.fr>
2019-11-17 10:27:04 +0000

    Re-sync

---
+++ db_infos.php
@@ -0,0 +1,9 @@
+<!-- db_infos.php : informations de connexion à la base de données -->
+
+<?php
+  $db_server = "localhost";
+  $db_username ="remi";
+  $db_password = "badPassword";
+  $db_database = "remidb";
+  $db_table = "MEMORY_USERS";
+?>
